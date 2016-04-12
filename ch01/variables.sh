b123af0d2846e6db09946bc1005078204ebf5367
noprom <tyee.noprom@qq.com>
2016-04-12 12:25:54 +0800

    add root user check

--- ch01/variables.sh
+++ ch01/variables.sh
@@ -3,3 +3,18 @@
 fruit=apple
 count=5
 echo "We have $count ${fruit}(s)"
+
+# 获取变量的长度
+var=12345678901234567890$
+echo ${#var}
+
+# 当前的SHELL
+echo $SHELL
+
+# 检查是否是root用户
+if [ $UID -ne 0 ];
+    then
+      echo Non root user. Please run as root.
+     else
+       echo Root user
+fi
